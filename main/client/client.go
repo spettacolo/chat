06cba086d5b3a3b8b708785cb410f84b65d17f78
spettacolo <alexfasolo2004@gmail.com>
2024-10-16 23:04:19 +0200

    Refactor client and server packages, update websocket connection handling

--- main/client/client.go
+++ main/client/client.go
@@ -2,14 +2,54 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
 
 func main() {
-	conn, _, err := websocket.DefaultDialer.Dial("ws://localhost:8080/ws", nil)
+	conn, _, err := websocket.DefaultDialer.Dial("ws://localhost:8080", nil)
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
 	defer conn.Close()
+
+	// Set up a channel to listen for interrupt signals to close the connection
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	// Send "hello world" message
+	err = conn.WriteMessage(websocket.TextMessage, []byte("hello world"))
+	if err != nil {
+		log.Println("write:", err)
+		return
+	}
+
+	// Close the connection by sending a close message and then
+	// waiting (with timeout) for the server to close the connection.
+	err = conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	if err != nil {
+		log.Println("write close:", err)
+		return
+	}
+	select {
+	case <-done:
+	case <-time.After(time.Second):
+	}
 }
--- main/server/server.go
+++ main/server/server.go
@@ -4,37 +4,53 @@ import (
 	"log"
 	"net/http"
 
-	//gin "github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		// Allow all connections by default
+		return true
+	},
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println(err)
+		log.Printf("Failed to upgrade connection: %v", err)
 		return
 	}
+	defer conn.Close()
 
 	for {
 		messageType, p, err := conn.ReadMessage()
 		if err != nil {
-			log.Println(err)
-			return
+			if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+				log.Printf("Normal closure: %v", err)
+			} else if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("Unexpected close error: %v", err)
+			} else {
+				log.Printf("Read error: %v", err)
+			}
+			break
 		}
 
+		// Print the received message
+		log.Printf("Received message: %s", p)
+
 		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
+			log.Printf("Write error: %v", err)
+			break
 		}
 	}
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	log.Println("Server started on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
