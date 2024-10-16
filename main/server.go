7309b061aafc2a4939f52bd61f526dbc5e5de725
spettacolo <alexfasolo2004@gmail.com>
2024-10-16 22:31:33 +0200

    update

--- main/server.go
+++ main/server.go
@@ -3,16 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+
 	//gin "github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize := 1024,
-	WriteBufferSize := 1024,
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
-func handler(w http.ResponseWriter, r *http.Request){
+func handler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
@@ -22,15 +23,18 @@ func handler(w http.ResponseWriter, r *http.Request){
 	for {
 		messageType, p, err := conn.ReadMessage()
 		if err != nil {
+			log.Println(err)
 			return
 		}
 
 		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
 			return
 		}
 	}
 }
 
 func main() {
-
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
